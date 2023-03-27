32ead8f337d41daf17d97b140f42318c8cab60ae
licask8 <elielson.elrick@gmail.com>
2023-03-27 19:42:22 -0300

    implementando feedback visual

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,6 +1,6 @@
 import { Dimensions , Text } from 'react-native';
 
-import Animated, { Keyframe} from 'react-native-reanimated'
+import Animated, { Keyframe, runOnJS} from 'react-native-reanimated'
 
 import { Option } from '../Option';
 import { styles } from './styles';
@@ -14,11 +14,12 @@ type Props = {
   question: QuestionProps;
   alternativeSelected?: number | null;
   setAlternativeSelected?: (value: number) => void;
+  onUnmount: () => void;
 }
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
-export function Question({ question, alternativeSelected, setAlternativeSelected }: Props) {
+export function Question({ question, alternativeSelected, setAlternativeSelected, onUnmount }: Props) {
 
   const enteringKeyFrame = new Keyframe({
     0: {
@@ -63,7 +64,12 @@ export function Question({ question, alternativeSelected, setAlternativeSelected
   return (
     <Animated.View style={styles.container}
      entering={enteringKeyFrame.duration(400)}
-     exiting={exitingKeyFrame.duration(400)}
+     exiting={exitingKeyFrame.duration(400).withCallback((finished) => {
+      'worklet'
+      if(finished){
+        runOnJS(onUnmount)();
+      }
+     })}
     >
       <Text style={styles.title}>
         {question.title}
--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -17,6 +17,7 @@ import { QuizHeader } from '../../components/QuizHeader';
 import { ProgressBar} from '../../components/ProgressBar'
 import { ConfirmButton } from '../../components/ConfirmButton';
 import { OutlineButton } from '../../components/OutlineButton';
+import { OverlayFeedback } from '../../components/OverlayFeedback';
 
 interface Params {
   id: string;
@@ -33,6 +34,8 @@ export function Quiz() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [quiz, setQuiz] = useState<QuizProps>({} as QuizProps);
   const [alternativeSelected, setAlternativeSelected] = useState<null | number>(null);
+
+  const [statusReply, setStatusReply] = useState(0);
 
   const shake = useSharedValue(0);
   const scrollY = useSharedValue(0);
@@ -91,14 +94,15 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      setStatusReply(1);
       setPoints(prevState => prevState + 1);
+      handleNextQuestion();
     } else {
+      setStatusReply(2)
       shakeAnimation();
     }
 
     setAlternativeSelected(null);
-
-    handleNextQuestion();
   }
 
   function handleStop() {
@@ -119,8 +123,13 @@ export function Quiz() {
 
   function shakeAnimation() {
     shake.value = withSequence(
-      withTiming(3, {duration: 400, easing: Easing.bounce}),
-      withTiming(0)
+      withTiming(3, {duration: 400, easing: Easing.bounce }),
+      withTiming(0, undefined, (finished) => {
+        'worklet';
+        if (finished){
+          runOnJS(handleNextQuestion)();
+        }
+      })
     );
   }
 
@@ -196,6 +205,8 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
+      <OverlayFeedback status={statusReply} />
+
       <Animated.View style={fixedProgressBarStyles}>
         <Text style={styles.title}>
           {quiz.title}
@@ -228,6 +239,7 @@ export function Quiz() {
                   question={quiz.questions[currentQuestion]}
                   alternativeSelected={alternativeSelected}
                   setAlternativeSelected={setAlternativeSelected}
+                  onUnmount={() => setStatusReply(0)}
                 />
           </Animated.View>    
        </GestureDetector>
